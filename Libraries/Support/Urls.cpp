c1e3c671f297de9be1ef6718b96d8a2117e845f8
Josh <dragonCASTjosh@gmail.com>
2020-10-18 11:18:42 +0200

    Update Docs Urls

--- Libraries/Support/Urls.cpp
+++ Libraries/Support/Urls.cpp
@@ -8,7 +8,7 @@ namespace Urls
 // Pagination is limited to 100, but we request more in case they ever change it
 // to a higher number.
 const cstr cUserPlasmaHub = "https://plasmagameengine.com";
-const cstr cUserDoc = "https://plasmagameengine.com";
+const cstr cUserDoc = "https://github.com/PlasmaEngine/PlasmaDocs";
 const cstr cUserRoadmap = "https://plasmagameengine.com";
 const cstr cUserCompany = "https://plasmagameengine.com";
 const cstr cUserQa = "https://plasmagameengine.com";
@@ -19,7 +19,7 @@ const cstr cUserLatestIssues = "https://github.com/PlasmaEngine/PlasmaEngine_Iss
 const cstr cUserSpecificIssue = "https://plasmagameengine.com";
 const cstr cUserChat = "https://discord.gg/Jrzb5ng";
 const cstr cUserLauncherDownloads = "https://plasmagameengine.com";
-const cstr cUserOnlineDocs = "https://plasmagameengine.com";
+const cstr cUserOnlineDocs = "https://github.com/PlasmaEngine/PlasmaDocs";
 const cstr cUserHelp = "https://plasmagameengine.com";
 const cstr cUserWebRequesterDefault = "https://www.example.com/";
 const cstr cApiBuilds = "https://api.github.com/repos/dragonCASTjosh/Plasma/releases?per_page=10000";
