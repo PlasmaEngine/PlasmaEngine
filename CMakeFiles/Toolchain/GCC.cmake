dcaa91d79a6deb0b23101828102e3a7e08591573
Josh <dragonCASTjosh@gmail.com>
2021-03-15 23:57:00 +0100

    Fix GCC CMake Issue

--- CMakeFiles/Toolchain/GCC.cmake
+++ CMakeFiles/Toolchain/GCC.cmake
@@ -1,3 +1,12 @@
 add_definitions(-PlasmaCompilerGcc=1 -DPlasmaCompilerName="GCC")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+
+function(plasma_toolchain_setup_library target)
+endfunction()
+
+function(plasma_use_precompiled_header target directory)
+endfunction()
+
+function(plasma_source_ignore_precompiled_header source)
+endfunction()
