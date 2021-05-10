0a7b4c54d821ab82d7410d75dc9ec4d248fe9bf6
Josh <dragonCASTjosh@gmail.com>
2021-05-10 18:52:45 +0100

    Dont install post build

--- build_vs2019_ninja.bat
+++ build_vs2019_ninja.bat
@@ -6,6 +6,6 @@ cd build\ninja
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 cmake ..\..\ -G "Ninja" -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_INSTALL_PREFIX="Sdk"
-cmake --build . --target install --config Release
+cmake --build . --config Release
 cd ..\..\
 pause
